e88311193dd55f2546e92972a61bd1d1efae085d
Paul Sandoval <paul13sand@gmail.com>
2026-03-06 20:52:19 -0400

    Implementacion de Models segun diagrama de clases

---
+++ TechZoneDesktop/TechZoneDesktop/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechZoneDesktop.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string CedulaIdentidad { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
---
+++ TechZoneDesktop/TechZoneDesktop/Models/DetalleVenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechZoneDesktop.Models
+{
+    public class DetalleVenta
+    {
+        public int Id { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal PrecioUnitarioBs { get; set; }
+
+        public decimal SubtotalBs { get; set; }
+
+        public int ProductoId { get; set; }
+
+        public int VentaId { get; set; }
+    }
+}
