b2fa5179555dcfb61838ec7ef0c31c80e369bcdd
Paul Sandoval <paul13sand@gmail.com>
2026-03-08 10:39:58 -0400

    Implementacion CRUD inicial de clientes

--- TechZoneDesktop/TechZoneDesktop/Controllers/ClienteController.cs
+++ TechZoneDesktop/TechZoneDesktop/Controllers/ClienteController.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechZoneDesktop.Models;
+using TechZoneDesktop.Services;
 
 namespace TechZoneDesktop.Controllers
 {
-    internal class ClienteController
+    public class ClienteController
     {
+        ClienteService clienteService = new ClienteService();
+
+        public void GuardarCliente(Cliente cliente)
+        {
+            clienteService.InsertarCliente(cliente);
+        }
     }
 }
--- TechZoneDesktop/TechZoneDesktop/Services/ClienteService.cs
+++ TechZoneDesktop/TechZoneDesktop/Services/ClienteService.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TechZoneDesktop.Models;
+using TechZoneDesktop.Data;
 
 namespace TechZoneDesktop.Services
 {
-    internal class ClienteService
+    public class ClienteService
     {
+        DatabaseConnection db = new DatabaseConnection();
+
+        public void InsertarCliente(Cliente cliente)
+        {
+            using (SqlConnection conn = db.GetConnection())
+            {
+                conn.Open();
+
+                string query = "INSERT INTO Cliente (Nombre, Apellido, CedulaIdentidad, Telefono) VALUES (@Nombre, @Apellido, @Cedula, @Telefono)";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@Nombre", cliente.Nombre);
+                cmd.Parameters.AddWithValue("@Apellido", cliente.Apellido);
+                cmd.Parameters.AddWithValue("@Cedula", cliente.CedulaIdentidad);
+                cmd.Parameters.AddWithValue("@Telefono", cliente.Telefono);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
